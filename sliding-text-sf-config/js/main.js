a3376393ab01a0654a8d60453920cd00d3312d94
singleserveapps <alan.acheff@gmail.com>
2015-10-02 09:37:34 -0700

    Changed to document.getElementById

--- sliding-text-sf-config/js/main.js
+++ sliding-text-sf-config/js/main.js
@@ -1,30 +1,21 @@
-(function () {
-	loadOptions();
-	submitHandler();
-})();
+function getConfigData() {
+    var backgroundColorPicker = document.getElementById('backgroundColorPicker');
+    var textColorPicker = document.getElementById('textColorPicker');
+    var displayWeather = document.getElementById('displayWeather');
+ 
+    var options = {
+      'backgroundColorPicker': backgroundColorPicker.value,
+      'textColorPicker': textColorPicker.value
+      'displayWeather': displayWeather.checked
 
-function submitHandler() {
-	var $submitButton = $('#submitButton');
+    // Save for next launch
+    localStorage['backgroundColorPicker'] = options['backgroundColorPicker'];
+    localStorage['textColorPicker'] = options['textColorPicker'];
+    localStorage['displayWeather'] = options['displayWeather'];
 
-	$submitButton.on('click', function() {
-		console.log('Submit');
-
-		var return_to = getQueryParam('return_to', 'pebblejs://close#');
-		document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
-	});
-}
-
-function loadOptions() {
-	var $backgroundColorPicker = $('#backgroundColorPicker');
-	var $textColorPicker = $('#textColorPicker');
-	var $displayWeather = $('#displayWeather');
-
-	if (localStorage.backgroundColor) {
-		$backgroundColorPicker[0].value = localStorage.backgroundColor;
-		$textColorPicker[0].value = localStorage.textColor;
-		$displayWeather.checked = localStorage.displayWeather;
-	}
-}
+    console.log('Got options: ' + JSON.stringify(options));
+    return options;
+};
 
 function getAndStoreConfigData() {
 	var $backgroundColorPicker = $('#backgroundColorPicker');
@@ -44,6 +35,29 @@ function getAndStoreConfigData() {
 	console.log('Got options: ' + JSON.stringify(options));
 	return options;
 }
+
+  var submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', function() {
+    console.log('Submit');
+
+    // Set the return URL depending on the runtime environment
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(getConfigData()));
+  });
+
+  (function() {
+    var backgroundColorPicker = document.getElementById('backgroundColorPicker');
+    var textColorPicker = document.getElementById('textColorPicker');
+    var displayWeather = document.getElementById('displayWeather');
+
+    // Load any previously saved configuration, if available
+    if(localStorage['displayWeather']) {
+
+      displayWeather.checked = JSON.parse(localStorage['displayWeather']);
+      backgroundColorPicker.value = localStorage['backgroundColorPicker'];
+      textColorPicker.value = localStorage['textColorPicker'];
+    }
+  })();
 
 function getQueryParam(variable, defaultValue) {
   var query = location.search.substring(1);
