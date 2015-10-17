7ea183fabd6f812dec036aa85b8a1ff1178ecf19
singleserveapps <alan.acheff@gmail.com>
2015-10-16 21:27:32 -0700

    Update main.js

--- sliding-time-beta-config/js/main.js
+++ sliding-time-beta-config/js/main.js
@@ -3,6 +3,21 @@
 	submitHandler();
 })();
 
+$(function(){
+
+	var currentValue = $('#currentValue');
+
+	$('#defaultSlider').change(function(){
+	    currentValue.html(this.value);
+	});
+
+	// Trigger the event on load, so
+	// the value field is populated:
+
+	$('#defaultSlider').change();
+
+});
+
 function submitHandler() {
 	var $submitButton = $('#submitButton');
 
