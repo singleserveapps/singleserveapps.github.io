d348d3501947c7ef3d9dcda76628615a008ad749
singleserveapps <alan.acheff@gmail.com>
2015-10-16 21:09:53 -0700

    Update main.js

--- sliding-time-beta-config/js/main.js
+++ sliding-time-beta-config/js/main.js
@@ -29,7 +29,7 @@ function loadOptions() {
 		$backgroundColorPicker[0].value = localStorage.backgroundColor;
 		$textColorPicker[0].value = localStorage.textColor;
 		$weatherFrequencySlider.val(localStorage.weatherFrequency);
-		$weatherFrequencySlider.text = localStorage.weatherFrequency;
+		$weatherFrequencySlider.text = $weatherFrequencySlider.val();
 		$weatherDateAlignment.val("1");
 		$weatherDateAlignment.val(localStorage.weatherDateAlignment);
 		$hourMinutesAlignment.val("0");
